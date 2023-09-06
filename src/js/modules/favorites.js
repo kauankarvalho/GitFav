05994f6571f06c88e129ebd2397ae6b71df4a2f1
kauankarvalho <kauan.karvalho@outlook.com>
2023-09-06 14:54:39 -0300

    refactor: separate FavoritesView class into a new module

--- src/js/modules/favorites.js
+++ src/js/modules/favorites.js
@@ -1,6 +1,6 @@
 import { GithubUserAPI } from "./githubUserAPI.js"
 
-class Favorites {
+export class Favorites {
   constructor(root) {
     this.root = document.querySelector(root)
     this.entries = []
@@ -30,95 +30,5 @@ class Favorites {
       this.entries = this.entries.filter((user) => user.login !== username)
       this.update()
     }
-  }
-}
-
-export class FavoritesView extends Favorites {
-  constructor(root) {
-    super(root)
-
-    this.input = document.querySelector("#search")
-    this.button = document.querySelector("#add-favorite")
-
-    this.event()
-  }
-
-  event() {
-    this.button.addEventListener("click", () => {
-      this.add(this.input.value)
-    })
-  }
-
-  createRow() {
-    const tr = document.createElement("tr")
-    tr.classList.add(
-      "flex",
-      "items-center",
-      "gap-[4rem]",
-      "px-[4rem]",
-      "py-[1.6rem]",
-      "text-left",
-      "last:rounded-b-[1.2rem]",
-      "odd:bg-[rgba(6,_22,_27,_0.5)]",
-      "even:bg-[#06181C]",
-    )
-    return tr
-  }
-
-  update() {
-    this.root.innerHTML = ""
-    this.input.value = ""
-
-    if (this.entries.length === 1) {
-      const noFavorites = document.querySelector("#no-favorites")
-      noFavorites.classList.add("hidden")
-    } else if (this.entries.length === 0) {
-      const noFavorites = document.querySelector("#no-favorites")
-      noFavorites.classList.remove("hidden")
-    }
-
-    this.entries.forEach((user) => {
-      const tr = this.createRow()
-
-      if (user.name === null) {
-        user.name = "Usuário sem nome"
-      }
-
-      tr.innerHTML = `
-        <th class="flex basis-3/4 items-center gap-[1.8rem]">
-          <img
-            class="h-[5.6rem] w-[5.6rem] rounded-full"
-            src="${user.avatar_url}"
-            alt="Imagem de ${user.name}"
-          />
-          <div class="flex flex-col">
-            <span>${user.name}</span>
-            <a
-              class="font-normal"
-              href="${user.html_url}"
-              target="_blank"
-            >
-              ${user.login}
-            </a>
-          </div>
-        </th>
-
-        <th class="basis-1/4">${user.public_repos}</th>
-        <th class="basis-1/4">${user.followers}</th>
-
-        <th
-          class="basis-1/6 text-[#F75A68]"
-        >
-          <button id="remove-favorite">Remover</button>
-        </th>
-      `
-
-      this.root.append(tr)
-
-      const removeButton = tr.querySelector("#remove-favorite")
-      removeButton.addEventListener("click", () => {
-        this.remove(user.login)
-      })
-    })
   }
 }
